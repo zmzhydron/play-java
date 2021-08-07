74f00b3713933cf62333cb13de98210dd4f5af9a
mingzhizhang <mingzhizhang@healthlink.cn>
2021-08-07 22:00:42 +0800

    111

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+public class HelloWorld {
+    String nigger;
+    // Date niggerDate<Date> = new Date();
+    public HelloWorld(){
+        System.out.println("fuckyou nigger");
+    }
+    public void setNigger(String newname){
+        nigger = newname;
+    }
+    public String getnigger(){
+        return nigger;
+    }
+    public static void main(String[] args) {
+        System.out.println("~~~~~~~~~~~~~Hello World~~~~~~~~~~~~~");
+        HelloWorld nigger = new HelloWorld();
+        nigger.setNigger("kendra lust");
+        System.out.println(nigger.getnigger());
+        System.out.println(new Date());
+    }
+}
